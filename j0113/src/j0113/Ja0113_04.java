1ec14de97eeb8de2178811a81ccbc35baaca3914
KOSMO <KOSMO@DESKTOP-QG52G94>
2025-01-14 17:33:59 +0900

    1월13일

---
+++ j0113/src/j0113/Ja0113_04.java
@@ -0,0 +1,38 @@
+package j0113;
+
+public class Ja0113_04 {
+
+	public static void main(String[] args) {
+		//문자 - g 저장해서 출력
+		//문자열 - "안녕하세요." 출력
+		// 숫자 - 10000 입력해서 출력
+		
+		char a ='g';
+		System.out.println(a);
+				
+		String str = "안녕하세요.";
+		System.out.println(str);
+		
+		int b =10000;
+		System.out.println(b);
+		
+		int true_temp = 10;
+		int up7 = 10;
+		
+		int c = 10;
+		int b$ = 10;
+		
+		int last_index_of = 10;
+		int lastIndexOf = 10;
+		
+		int pi = 3;
+		final int PI = 3; //상수 - 값을 변경할수 없음.
+		
+		pi = 10;
+		pi = 100;
+		pi = 1000;
+		
+
+	}
+
+}
---
+++ j0113/src/j0113/Ja0113_05.java
@@ -0,0 +1,41 @@
+package j0113;
+
+public class Ja0113_05 {
+
+	public static void main(String[] args) {
+		
+		//boolean - true,false
+		//char - '표시, 아스키코드값 : 65-'A'
+		char ch ='A';
+		System.out.println(ch);
+		
+		char ch2 ='\u0041'; // 16*4 + 1*1 = 65
+		System.out.println(ch2);
+		
+		char ch3 = 97; // 97 - a
+		System.out.println(ch3);
+		
+		char ch4 = '\t';
+		System.out.print(ch4);
+		
+		char ch5 ='a';
+		System.out.print(ch5);
+		
+		char ch6 ='b';
+		System.out.print(ch6);
+
+		char ch7 ='c';
+		System.out.print(ch7);
+		
+		boolean p =true;
+		boolean p2 = false;
+		
+		long l = 10000000000L;
+		
+		float f = 3.14F;
+		double d = 3.14D;
+
+
+	}
+
+}
